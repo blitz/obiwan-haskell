63b861a07519d24db104662963981bdef59d89de
Julian Stecklina <js@alien8.de>
2019-03-09 19:09:01 +0100

    Some rough code for timeout handling

--- src/Main.hs
+++ src/Main.hs
@@ -5,10 +5,13 @@ module Main (main) where
 import           Control.Monad             (void)
 import           Data.ByteString           as B
 import           Data.Functor              ((<&>))
+import           Data.List                 ((\\))
 import           Data.Map.Strict           as Map
 import           Debug.Trace               (trace)
 import qualified Network.Socket            as S
 import qualified Network.Socket.ByteString as SB
+import           System.Clock
+
 import           TftpProto
 
 -- Configuration
@@ -22,15 +25,16 @@ tftpTestData = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem a
 tftpBlockSize :: Int
 tftpBlockSize = 512
 
--- State Handling
+-- TFTP State Handling
 
-newtype Connection = Reading B.ByteString
+data Connection = Pristine
+                | Reading B.ByteString
 
 type Client = S.SockAddr
-type State = Map.Map Client Connection
+type TftpState = Map.Map Client Connection
 
-emptyState :: State
-emptyState = Map.empty
+emptyTftpState :: TftpState
+emptyTftpState = Map.empty
 
 -- Get the nth data block for a connection. Block numbers start at 1.
 getDataBlock :: Int -> B.ByteString -> Request
@@ -39,30 +43,52 @@ getDataBlock n = DTA (fromIntegral n) . getData
 
 continueConnection :: Request -> Connection -> Maybe (Connection, Request)
 continueConnection (ACK n) con@(Reading buf) = Just (con, getDataBlock (fromIntegral n + 1) buf)
+continueConnection (RRQ _ Binary) _ = continueConnection (ACK 0) (Reading tftpTestData)
 continueConnection _ _ = Nothing
 
 initiateConnection :: Client -> Request -> Maybe (Connection, Request)
-initiateConnection _ (RRQ _ Binary) = continueConnection (ACK 0) (Reading tftpTestData)
-initiateConnection _ _ = Nothing
+initiateConnection c req = trace (show c ++ ": " ++ show req) (continueConnection req Pristine)
 
-initiateConnectionWithLog :: Client -> Request -> Maybe (Connection, Request)
-initiateConnectionWithLog c req = trace ("New connection from " ++ show c ++ " " ++ show req) (initiateConnection c req)
-
-handleMessage :: State -> Client -> Request -> (State, Maybe Request)
+handleMessage :: TftpState -> Client -> Request -> (TftpState, Maybe Request)
 handleMessage s client req = maybe new continue (Map.lookup client s)
   where
     continue con = maybe (delete client s, Nothing) store (continueConnection req con)
-    new = maybe failed store (initiateConnectionWithLog client req)
+    new = maybe failed store (initiateConnection client req)
     failed = errorOut s IllegalOperation "Establishing connection failed"
     store (connection, resp) = (Map.insert client connection s, Just resp)
-    errorOut ns errCode msg = (ns, Just (ERR errCode msg))
+    errorOut ns errCode msg =
+      trace (show client ++ ": " ++ show msg) (ns, Just (ERR errCode msg))
+
+-- Timeout Handling
+-- TODO Retransmissions
+
+-- We use the boottime clock, because it is monotonic and also counts sleep times.
+timeoutClock :: Clock
+timeoutClock = Boottime
+
+-- After this much of silence from a client, we forget connection state.
+connectionTimeout :: TimeSpec
+connectionTimeout = TimeSpec 10 0
+
+-- Remember the time of the last packet of a client
+-- TODO This should be a priority queue
+type TimeoutState = [(Client, TimeSpec)]
+
+updateTimeout :: TimeoutState -> Client -> TimeSpec -> TimeoutState
+updateTimeout s client now = (client, now) : Prelude.filter isReplaced s
+  where isReplaced (c, _) = c == client
+
+processTimeouts :: TimeoutState -> TimeSpec -> ([Client], TimeoutState)
+processTimeouts s now = (Prelude.map fst oldEntries, s Data.List.\\ oldEntries)
+  where oldEntries = Prelude.filter isOld s
+        isOld (_, time) = (now - time) > connectionTimeout
 
 -- Control Flow
 
 maybeDo :: Maybe a -> (a -> IO ()) -> IO ()
 maybeDo m f = maybe (return ()) f m
 
-loopForever :: State -> (State -> IO State) -> IO ()
+loopForever :: TftpState -> (TftpState -> IO TftpState) -> IO ()
 loopForever s f = void (loopForever_ s)
   where loopForever_ s_ = f s_ >>= (`loopForever` f)
 
@@ -73,11 +99,13 @@ main = S.withSocketsDo $ do
               (Just "127.0.0.1") (Just tftpServerPort)
   S.bind sock (S.addrAddress addrInfo)
 
-  loopForever emptyState $ \state -> do
+  loopForever emptyTftpState $ \state -> do
     (msg, client) <- SB.recvFrom sock 1500
+    now <- getTime timeoutClock
+    -- TODO Update timeouts
     case decodePacket msg <&> handleMessage state client of
-      Just (newState, maybeReq) -> do
+      Just (newTftpState, maybeReq) -> do
         maybeDo maybeReq (\buf -> SB.sendAllTo sock (encodePacket buf) client)
-        return newState
+        return newTftpState
       Nothing ->
         return state
