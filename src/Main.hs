5f475065677b4092be246c4833973e168982f0dd
Julian Stecklina <js@alien8.de>
2019-03-09 17:34:53 +0100

    Implement connection state handling

--- src/Main.hs
+++ src/Main.hs
@@ -16,15 +16,12 @@ tftpServerPort :: String
 tftpServerPort = "12345"
 
 tftpTestData :: B.ByteString
-tftpTestData = "This is very long test data"
+tftpTestData = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+
+tftpBlockSize :: Int
+tftpBlockSize = 512
 
 -- State Handling
-
-getReply :: Request -> Maybe Request
-getReply (RRQ _ Ascii) = Just (ERR IllegalOperation "ASCII mode not supported")
-getReply (RRQ _ Binary) = Just (ERR FileNotFound "No such file")
-getReply (ACK _)          = Nothing
-getReply _                = Nothing
 
 data Connection = Reading B.ByteString
 
@@ -34,8 +31,23 @@ type State = Map.Map Client Connection
 emptyState :: State
 emptyState = Map.empty
 
+-- Get the nth data block for a connection. Block numbers start at 1.
+getDataBlock :: Int -> B.ByteString -> Request
+getDataBlock n = DTA (fromIntegral n) . getData
+  where getData = B.take tftpBlockSize . B.drop ((n - 1) * tftpBlockSize)
+
+initiateConnection :: Request -> Maybe (Connection, Request)
+initiateConnection (RRQ _ Binary) = Just (Reading tftpTestData, firstData)
+  where firstData = getDataBlock 1 tftpTestData
+initiateConnection _ = Nothing
+
 handleMessage :: State -> Client -> Request -> (State, Maybe Request)
-handleMessage s c r = (s, getReply r)
+handleMessage s c r = maybe newConnection continueConnection (Map.lookup c s)
+  where
+    continueConnection _ = (s, Just (ERR IllegalOperation "Not implemented"))
+    newConnection = maybe failedConnection storeConnection (initiateConnection r)
+    failedConnection = (s, Just (ERR IllegalOperation "Establishing connection failed"))
+    storeConnection (connection, resp) = (Map.insert c connection s, Just resp)
 
 -- Control Flow
 
