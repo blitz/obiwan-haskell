c13cbaf59992683b709cf19608cdd032cdfe4107
Julian Stecklina <js@alien8.de>
2019-03-09 03:56:30 +0100

    Switch parsing to Data.Binary and send simple responses

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,38 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
 
+module Main (main) where
+
+import           Control.Monad             (forever, void)
+import           Data.Binary               (decode, encode, get)
+import qualified Data.ByteString           as B
+import qualified Data.ByteString.Lazy      as BL
+import           Data.Text.Encoding        (encodeUtf8)
+import           Debug.Trace               (traceShow)
 import qualified Network.Socket            as S
 import qualified Network.Socket.ByteString as SB
+import           TftpProto
 
-tftpServerPort :: S.PortNumber
-tftpServerPort = 12345
+tftpServerPort :: String
+tftpServerPort = "12345"
+
+getReply :: Request -> Maybe Request
+getReply (RRQ filename _) = Just $ DTA 1 (encodeUtf8 "Foo!")
+getReply (ACK _)          = Nothing
+getReply _                = Nothing
+
+
+parseTftp :: B.ByteString -> Request
+parseTftp  = decode . BL.fromStrict
 
 main :: IO ()
 main = S.withSocketsDo $ do
   sock <- S.socket S.AF_INET S.Datagram 0
   addrInfo:_ <- S.getAddrInfo (Just (S.defaultHints { S.addrSocketType = S.Datagram}))
-              (Just "127.0.0.1") (Just "12345")
+              (Just "127.0.0.1") (Just tftpServerPort)
   S.bind sock (S.addrAddress addrInfo)
 
-  loop sock
-  where
-    loop sock = do
-      (mesg, client) <- SB.recvFrom sock 1500
-      -- TODO Do something with the message
-      _ <- SB.sendTo sock mesg client
-      loop sock
+  forever $ do
+    (msg, client) <- SB.recvFrom sock 1500
+    case getReply $ parseTftp msg of
+      Just reply -> void (SB.sendTo sock (BL.toStrict (encode reply)) client)
+      Nothing    -> return ()
---
+++ src/TftpProto.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module TftpProto where
+
+import           Data.Binary
+import           Data.Binary.Get
+import           Data.Binary.Put
+import qualified Data.ByteString      as B
+import qualified Data.ByteString.Lazy as BL
+import           Data.String          (fromString)
+import           Data.Text            (Text)
+import           Data.Text.Encoding   (decodeUtf8, encodeUtf8)
+import           Data.Word            (Word16)
+
+data Request = RRQ !Text !Text
+             | WRQ !Text !Text
+             | DTA !Word16 !B.ByteString
+             | ACK !Word16
+             | ERR !Word16 !Text
+  deriving (Show)
+
+putZeroTermString :: Text -> Put
+putZeroTermString t = putByteString (encodeUtf8 t) >> putWord8 0
+
+getZeroTermString :: Get Text
+getZeroTermString = decodeUtf8 . BL.toStrict <$> getLazyByteStringNul
+
+instance Binary Request where
+  put (RRQ filename mode) = do
+    putWord16be 1
+    putZeroTermString filename
+    putZeroTermString mode
+
+  put (WRQ filename mode) = do
+    putWord16be 2
+    putZeroTermString filename
+    putZeroTermString mode
+
+  put (DTA block fData) = do
+    putWord16be 3
+    putWord16be block
+    putByteString fData
+
+  put (ACK block) = do
+    putWord16be 4
+    putWord16be block
+
+  put (ERR errCode errMsg) = do
+    putWord16be 5
+    putWord16be errCode
+    putZeroTermString errMsg
+
+  get = do
+    opcode <- getWord16be
+    case opcode of
+      1 -> RRQ <$> getZeroTermString <*> getZeroTermString
+      2 -> WRQ <$> getZeroTermString <*> getZeroTermString
+      3 -> DTA <$> getWord16be <*> (BL.toStrict <$> getRemainingLazyByteString)
+      4 -> ACK <$> getWord16be
+      5 -> ERR <$> getWord16be <*> getZeroTermString
+      _ -> fail "Unknown opcode"
