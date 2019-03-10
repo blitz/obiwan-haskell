9ad796cf70546ad647bb4e7f0ab5c10ff2a93bff
Julian Stecklina <js@alien8.de>
2019-03-10 19:54:19 +0100

    Some hindent changes

--- src/TftpServer.hs
+++ src/TftpServer.hs
@@ -21,15 +21,16 @@ tftpMaxPacketSize :: Int
 tftpMaxPacketSize = 9000
 
 tftpTestData :: B.ByteString
-tftpTestData = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+tftpTestData =
+  "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
 
 tftpBlockSize :: Int
 tftpBlockSize = 512
 
 -- TFTP State Handling
-
-data Connection = Pristine
-                | Reading B.ByteString
+data Connection
+  = Pristine
+  | Reading B.ByteString
   deriving (Eq, Show)
 
 type Client = S.SockAddr
@@ -67,8 +68,11 @@ handleClient client socket = handleReceivedData Pristine
 createBoundUdpSocket :: String -> String -> IO S.Socket
 createBoundUdpSocket address service = do
   sock <- S.socket S.AF_INET S.Datagram 0
-  addrInfo:_ <- S.getAddrInfo (Just (S.defaultHints { S.addrSocketType = S.Datagram}))
-                (Just address) (Just service)
+  addrInfo:_ <-
+    S.getAddrInfo
+      (Just (S.defaultHints {S.addrSocketType = S.Datagram}))
+      (Just address)
+      (Just service)
   S.bind sock (S.addrAddress addrInfo)
   return sock
 
