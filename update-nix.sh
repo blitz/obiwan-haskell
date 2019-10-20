bd4026c081f05397276cb876b70bb0bf99fe8e30
Julian Stecklina <js@alien8.de>
2019-10-20 23:06:57 +0200

    Update haskell.nix as part of package update

--- update-nix.sh
+++ update-nix.sh
@@ -2,5 +2,7 @@
 
 # TODO Use nix-shell here to get dependencies.
 
-stack-to-nix --output . --stack-yaml stack.yaml
+set -eux
 
+nix-prefetch-git --quiet https://github.com/input-output-hk/haskell.nix > nix/haskell-nix-src.json
+stack-to-nix --output . --stack-yaml stack.yaml
